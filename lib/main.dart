5bc06c3d4272451b0a4ae60b14ef8434c221008a
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2021-04-20 14:20:46 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,104 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/a': (BuildContext context) => MyHomePage(title: 'page A'),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int counter = 0;
-
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: Colors.red);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home : 0',
-      style: optionStyle,
-    ),
-    Text(
-      'Business : 1',
-      style: optionStyle,
-    ),
-    Text(
-      'School : 2',
-      style: optionStyle,
-    ),
-    Text(
-      'Settings : 3',
-      style: optionStyle,
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.amber[800],
-      ),
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 6.0,
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter = counter - 1;
-          });
-          print('Floating button clicked $counter');
-        },
-        child: Icon(Icons.wifi),
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.yellow,
-        elevation: 6.0,
-        focusColor: Colors.amber,
-      ),
-    );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-
-    });
   }
 }
---
+++ lib/secondpage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SecondPage());
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+              children: [
+                Text('Second Page'),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Back'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
